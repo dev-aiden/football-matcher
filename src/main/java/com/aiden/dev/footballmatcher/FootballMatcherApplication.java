45545b057f702b8c78dccc351b39679e3caf86e7
dev-aiden <tyjk32@gmail.com>
2022-05-16 20:59:40 +0900

    Initial commit

---
+++ src/main/java/com/aiden/dev/footballmatcher/FootballMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.aiden.dev.footballmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aiden/dev/footballmatcher/FootballMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiden.dev.footballmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
