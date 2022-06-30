b5e533bcbd26159cbf9fe01fa3b3c274d1ec95d2
dev-aiden <tyjk32@gmail.com>
2022-06-30 21:42:53 +0900

    Add remember me

--- src/test/java/com/aiden/dev/footballmatcher/modules/account/AccountControllerTest.java
+++ src/test/java/com/aiden/dev/footballmatcher/modules/account/AccountControllerTest.java
@@ -4,6 +4,7 @@ import com.aiden.dev.footballmatcher.modules.account.validator.SignUpFormValidat
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureTestDatabase
 @WebMvcTest(AccountController.class)
 class AccountControllerTest {
 
--- src/test/java/com/aiden/dev/footballmatcher/modules/main/MainControllerTest.java
+++ src/test/java/com/aiden/dev/footballmatcher/modules/main/MainControllerTest.java
@@ -1,11 +1,15 @@
 package com.aiden.dev.footballmatcher.modules.main;
 
+import com.aiden.dev.footballmatcher.modules.account.AccountService;
 import com.aiden.dev.footballmatcher.modules.account.WithAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -14,6 +18,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MainControllerTest {
 
     @Autowired MockMvc mockMvc;
+    @MockBean AccountService accountService;
+    @MockBean DataSource dataSource;
 
     @DisplayName("index 페이지 보이는지 테스트 - 비회원")
     @Test
